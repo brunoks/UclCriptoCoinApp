88660df27abaf58cb995344568fa1e64a052b412
Bruno Vieira <bruno@mobilesaude.com.br>
2019-05-28 23:49:04 -0300

    trocando rota minerador

--- uclcoin/minerador.py
+++ uclcoin/minerador.py
@@ -3,7 +3,7 @@ import requests
 import json
 from collections import namedtuple
 
-r = requests.get('http://127.0.0.1:5000/block/minable/0382499de4b7f5ffd6a86dd71c94364ba5f6682f7434304693f0df9130442dc072')
+r = requests.get('https://uclcriptocoin.herokuapp.com/block/minable/0382499de4b7f5ffd6a86dd71c94364ba5f6682f7434304693f0df9130442dc072')
 last_block = json.loads(r.text)
 block = Block.from_dict(last_block["block"])
 difficulty = last_block["difficulty"]
@@ -14,4 +14,4 @@ while block.current_hash[:difficulty].count('0') < difficulty:
 
 data = json.dumps(block, default=lambda x: x.__dict__)
 
-requests.post('http://127.0.0.1:5000/block',data,json=True)
+requests.post('https://uclcriptocoin.herokuapp.com/block',data,json=True)
