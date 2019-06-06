3f5bcacadc9d989a3f18b21b0f786ef237f25b9b
Bruno Vieira <bruno@mobilesaude.com.br>
2019-06-05 21:41:15 -0300

    add transaction json

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ def add_transaction2(private_key, public_key, value):
 
         for node in json.loads(get_nodes()):
             print(dict(transaction))
-            status = (requests.post(f'{node["address"]}/transaction', data=json.loads(dict(transaction))))
+            status = (requests.post(f'{node["address"]}/transaction', data=json.loads(transaction)))
             print(status)
 
         blockchain.add_transaction(transaction)
