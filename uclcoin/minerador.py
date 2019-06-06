990f18b256966cbba971ab2151692e7bd3e5db14
Bruno Vieira <bruno@mobilesaude.com.br>
2019-06-06 19:52:45 -0300

    fix

--- uclcoin/minerador.py
+++ uclcoin/minerador.py
@@ -4,40 +4,52 @@ import json
 from collections import namedtuple
 
 
-class Minerador(object):
+r = requests.get('https://uclcriptocoin.herokuapp.com/block/minable/03ecd19ebc9454eb635670577c24ad34a61fa05c23ce47f142fe704f6b37f59708')
+last_block = json.loads(r.text)
+block = Block.from_dict(last_block["block"])
+difficulty = last_block["difficulty"]
 
-    def minerarBloco(self, address):
+while block.current_hash[:difficulty].count('0') < difficulty:
+    block.nonce += 1
+    block.recalculate_hash()
 
-        wallet = KeyPair(address)
-        r = requests.get('https://uclcriptocoin.herokuapp.com/block/minable/' + wallet.public_key)
-        print(r.text)
-        last_block = json.loads(r.text)
-        block = Block.from_dict(last_block["block"])
-        difficulty = last_block["difficulty"]
+data = json.dumps(block, default=lambda x: x.__dict__)
 
-        while block.current_hash[:difficulty].count('0') < difficulty:
-            block.nonce += 1
-            block.recalculate_hash()
-
-
-        data = json.dumps(block, default=lambda x: x.__dict__)
-        print(data)
-
-        r = requests.post('https://uclcriptocoin.herokuapp.com/block',data=data,json=True)
-        print("ceguei",r.text)
-        self.pesquisarBlocoPendente()
-
-    def pesquisarBlocoPendente(self):
-        r = requests.get('https://uclcriptocoin.herokuapp.com/pending_transactions')
-
-        data = r.json()
-        if int(len(data['transactions']) > 0):
-            self.minerarBloco('10c3e7593eb0525c10652c835e85f8e709e897bf891ef9fd9451c94755690ccf')
-        else:
-            return 'Não tem mais bloco'
+requests.post('https://uclcriptocoin.herokuapp.com/block',data,json=True)
+print(requests)
 
 
-minerador = Minerador()
 
-current1 = minerador.minerarBloco('10c3e7593eb0525c10652c835e85f8e709e897bf891ef9fd9451c94755690ccf')
-#current2 = minerador.minerarBloco('85d7036e4451228c64951b78a16e9de0fd360c852f5b85601b1fbb7b0e322b8a')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
