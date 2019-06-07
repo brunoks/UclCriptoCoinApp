767f2a7e3b5b042e6779c24217b6f1a861e03609
Bruno Vieira <bruno@mobilesaude.com.br>
2019-06-06 22:51:21 -0300

    update deploy

--- uclcoin/minerador.py
+++ uclcoin/minerador.py
@@ -3,18 +3,35 @@ import requests
 import json
 from collections import namedtuple
 
-wallet = KeyPair("10c3e7593eb0525c10652c835e85f8e709e897bf891ef9fd9451c94755690ccf")
-r = requests.get('https://uclcriptocoin2.herokuapp.com/block/minable/' + wallet.public_key)
-print(r.text)
-last_block = json.loads(r.text)
-block = Block.from_dict(last_block["block"])
-difficulty = last_block["difficulty"]
+class Minerador(object):
 
-while block.current_hash[:difficulty].count('0') < difficulty:
-    block.nonce += 1
-    block.recalculate_hash()
+    def minerarBloco(self, address):
+        wallet = KeyPair(address)
+        r = requests.get('https://uclcriptocoin.herokuapp.com/block/minable/' + wallet.public_key)
+        print(r.text)
+        last_block = json.loads(r.text)
+        block = Block.from_dict(last_block["block"])
+        difficulty = last_block["difficulty"]
 
-data = json.dumps(block, default=lambda x: x.__dict__)
+        while block.current_hash[:difficulty].count('0') < difficulty:
+            block.nonce += 1
+            block.recalculate_hash()
 
-response = requests.post('https://uclcriptocoin2.herokuapp.com/block',data,json=True)
-print(response.text)
+        data = json.dumps(block, default=lambda x: x.__dict__)
+
+        r = requests.post('https://uclcriptocoin.herokuapp.com/block',data,json=True)
+        print(r.text)
+        #self.pesquisarBlocoPendente()
+
+    def pesquisarBlocoPendente(self):
+        r = requests.get('https://uclcriptocoin.herokuapp.com/pending_transactions')
+
+        data = r.json()
+        if int(len(data['transactions']) > 0):
+            self.minerarBloco('10c3e7593eb0525c10652c835e85f8e709e897bf891ef9fd9451c94755690ccf')
+        else:
+            return 'Não tem mais bloco'
+
+minerador = Minerador()
+
+minerador.minerarBloco("10c3e7593eb0525c10652c835e85f8e709e897bf891ef9fd9451c94755690ccf")
